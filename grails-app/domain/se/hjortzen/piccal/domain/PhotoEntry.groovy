b6f5d9fdaed68a779b617a2b5150efd5391da35c
Marcus Hjortzen <github@hjortzen.com>
2011-12-13 23:17:59 +0100

    Added some attributes to the User and PhotoEntry domain objects

--- grails-app/domain/se/hjortzen/piccal/domain/PhotoEntry.groovy
+++ grails-app/domain/se/hjortzen/piccal/domain/PhotoEntry.groovy
@@ -1,6 +1,12 @@
 package se.hjortzen.piccal.domain
 
 class PhotoEntry {
+    long id;
+    String description;
+    Date createDate;
+    Date targetDate;
+    String userId;
+    String originalUrl;
 
     static constraints = {
     }
--- grails-app/domain/se/hjortzen/piccal/domain/User.groovy
+++ grails-app/domain/se/hjortzen/piccal/domain/User.groovy
@@ -1,7 +1,11 @@
 package se.hjortzen.piccal.domain
 
 class User {
+    String id;
+    String description;
+    String email;
 
     static constraints = {
+        id(blank: false)
     }
 }
