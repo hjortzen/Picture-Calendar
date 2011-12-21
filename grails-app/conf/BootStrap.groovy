20e3bd36ca2d7919957d7397b217c89656b54a55
Marcus Hjortzen <github@hjortzen.com>
2011-12-22 00:55:32 +0100

    JSON object marshaller definitions for domain objects

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,27 @@
+import grails.converters.JSON
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = {servletContext ->
+        JSON.registerObjectMarshaller(se.hjortzen.piccal.domain.PhotoEntry) {
+            def returnArray = [:]
+            returnArray['id'] = it.id
+            returnArray['description'] = it.description
+            returnArray['createDate'] = it.createDate
+            returnArray['targetDate'] = it.targetDate
+            returnArray['originalUrl'] = it.originalUrl
+
+            return returnArray
+        }
+        JSON.registerObjectMarshaller(se.hjortzen.piccal.domain.Calendar) {
+            def returnArray = [:]
+            returnArray['id'] = it.id
+            returnArray['description'] = it.description
+
+            return returnArray
+        }
     }
+
     def destroy = {
     }
 }
